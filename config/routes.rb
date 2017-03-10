9bd6385a3331e7d94ec2a00674b8b4a7d8107c04
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-10 17:36:53 +0000

    basic form created

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  match '/contact_us',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
+  root to: 'contacts#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

42b8d9002c5064a4f770d368e0aea7d8ea131b6a
leyyaps <andrew.smith@ipsofactouk.com>
2017-11-17 10:15:21 +0000

    adjusted margins on albums

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   include Pundit
   after_action :verify_authorized, unless: :devise_controller?
+
+   def after_sign_in_path_for(resource)
+    redirect_to dashboard_path
+   end
 end

34a12a9c3eb4a6db6ffe8aae1a7a8b87b872d10e
leyyaps <andrew.smith@ipsofactouk.com>
2018-01-16 13:00:44 +0000

    attempt at recaptcha

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,12 +10,18 @@ class ContactsController < ApplicationController
     @albums = Album.all
 
     @contact = Contact.new(params[:contact])
-    
+
     # if verify_recaptcha(model: @contact) && @contact.save
     #   redirect_to @contact
     # else
     # render 'new'
     # end  
+   
+    if verify_recaptcha(model: @contact) && @contact.request
+      redirect_to @contact
+    else
+      render 'new'
+    end
 
     authorize @contact
     @contact.request = request
